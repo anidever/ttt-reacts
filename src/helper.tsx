882591478820cfb3b7857c736e2a649327323baa
anidever <anidever@outlook.com>
2021-01-04 22:47:28 -0500

    Rename lines to winners

--- src/helper.tsx
+++ src/helper.tsx
@@ -1,5 +1,5 @@
 export function calculateWinner(squares: number[]) {
-  const lines = [
+  const winners = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -10,8 +10,8 @@ export function calculateWinner(squares: number[]) {
     [2, 4, 6],
   ];
 
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (let i = 0; i < winners.length; i++) {
+    const [a, b, c] = winners[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
